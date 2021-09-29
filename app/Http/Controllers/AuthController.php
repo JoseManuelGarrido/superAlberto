4c7ffa6043bd3dce48f02c6554507c825c1cd06c
JoseManuelGarrido <josemcaballero78@gmail.com>
2021-09-29 21:30:31 +0200

    login terminado

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,10 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function login(Request $request){
+        //validamos los datos
+        $credentials = $request->validate([
+            'email'=>['required', 'email'],
+            'password'=>['required'],
+        ]);
+
+        //verificar que el email existe y que la contraseña es correcta
+        if(Auth::attempt($credentials)){
+            $usuarioLogeado = Auth::user();
+            $token = $usuarioLogeado->createToken('TokenUsuario')->plainTextToken;
+            $respuesta = [
+                'data'=>[
+                    'usuario'=>$usuarioLogeado,
+                    'token'=>$token,
+                ],
+            ];
+
+            return response()->json($respuesta);
+        } else{
+            return response()->json(['error'=>'Unauthorised'], 401);
+        }
+    }
+
     public function register(Request $request){
         //validar los datos
         $credentials = $request->validate([
